569e0f3de1aed918e37e739e9e8c79dc03e94469
Sianernian <191643041@qq.com>
2020-09-22 19:22:57 +0800

    alter

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "282725505@qq.com"
+	c.Data["Website"] = "https://www.baidu.com/"
+	c.Data["Email"] = "1916430401@qq.com"
 	c.TplName = "index.tpl"
 }
